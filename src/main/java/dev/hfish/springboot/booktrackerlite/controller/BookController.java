e4faf30f7b96523425f878637ce20805ad5128a1
haydenfish <hayden.fish@icloud.com>
2023-12-20 23:01:04 -0800

    Refactoring - removed staging for Oauth2 authentication 12.20.2023

--- src/main/java/dev/hfish/springboot/booktrackerlite/controller/BookController.java
+++ src/main/java/dev/hfish/springboot/booktrackerlite/controller/BookController.java
@@ -3,7 +3,6 @@ package dev.hfish.springboot.booktrackerlite.controller;
 import dev.hfish.springboot.booktrackerlite.entity.Book;
 import dev.hfish.springboot.booktrackerlite.service.book.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
